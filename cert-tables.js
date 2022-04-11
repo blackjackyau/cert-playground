6caf4a9a7dac11215e1a8f845c4238bc3c5f31d8
blackjackyau <yau.yik.shiung@gmail.com>
2022-04-11 23:03:44 +0800

    simple ui to perform cert chain evaluation

---
+++ cert-tables.js
@@ -0,0 +1,59 @@
+export default function CertTables(props) {
+  return (
+    <table class="table table-sm">
+    <thead>
+      <tr>
+        <th scope="col">Cert</th>
+        <th scope="col">Evaluation</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.certs.map(cert =>
+          (<tr>
+            <td className="w-50">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{cert.cert.getSubject().str}</h5>
+                <pre className="card-text text-break">{cert.cert.getInfo()}</pre>
+              </div>
+            </div>
+            <ul className="list-group">
+              <li className="list-group-item text-break">
+                <a className="btn btn-primary" data-bs-toggle="collapse" href="#colPK" role="button" aria-expanded="false" aria-controls="colPK">
+                  Public Key
+                </a>
+                <div className="collapse mt-1" id="colPK">
+                  {cert.cert.getPublicKeyHex()}
+                </div>
+              </li>
+              <li className="list-group-item text-break">
+                <a className="btn btn-primary" data-bs-toggle="collapse" href="#colSignature" role="button" aria-expanded="false" aria-controls="colSignature">
+                  Signature
+                </a>
+                <div className="collapse mt-1" id="colSignature">
+                {cert.cert.getSignatureValueHex()}
+                </div>
+              </li>
+            </ul>
+            </td>
+            <td className="w-50">
+              <div>
+                {cert.meta.selfSigned ? (<span class="badge bg-primary">Self Signed</span>) : undefined}
+                {cert.meta.caCert ? (<span class="badge bg-primary">CA Cert</span>) : undefined}
+                {cert.meta.signatureVerified ? (<span class="badge bg-success">Signature Verified</span>) : (<span class="badge bg-danger">Signature Not Verified</span>)}
+              </div>
+              {cert.meta.issuer ?
+                (<div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Issuer <br/> {cert.meta.issuer.getSubject().str}</h5>
+                    <pre className="card-text text-break">{cert.meta.issuer.getInfo()}</pre>
+                  </div>
+              </div>): (<span class="badge bg-danger">No issuer found</span>)}
+            </td>
+          </tr>
+          )
+      )}
+    </tbody>
+  </table>
+  );
+}
---
+++ index.js
@@ -0,0 +1,131 @@
+import CertTables from "cert-tables";
+
+
+function MainContent() {
+  const [caCerts, setCaCerts] = React.useState({});
+  const [formData, setFormData] = React.useState({
+    certs: ""
+  });
+
+  const [certs, setCerts] = React.useState();
+  const certHandler = new CertHandler();
+  
+  async function loadCert() {
+    const caBundlesResp = await fetch("ca-bundle.crt");
+    const caBundlesText = await caBundlesResp.text();
+
+    setCaCerts(() => {
+      const caCerts = certHandler.parse(caBundlesText);
+      window.caCerts = caCerts;
+      return caCerts;
+    });
+  }
+
+  function handleChange(event) {
+    setFormData(prev => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  function evaluate() {
+    setCerts(_ => {
+      let certs;
+      try {
+        certs = certHandler.parse(formData.certs);
+      } catch (ex) {
+        alert('Error parsing certs', ex);
+        return;
+      }
+
+      return Object.values(certs).map(cert => {
+        const result = { cert, meta: { selfSigned: false, signatureVerified: false, caCert: false, issuer: undefined }};
+
+        const issuer = cert.getIssuer().str;
+        const subject = cert.getSubject().str;
+
+        if (issuer === subject) {
+          result.meta.selfSigned = true;
+        }
+
+        result.meta.caCert = result.cert.getExtBasicConstraints() ? result.cert.getExtBasicConstraints()["cA"] : false; 
+
+        let issuerCert = certs[cert.getIssuer().str];
+        if (!issuerCert) {
+          issuerCert = caCerts[cert.getIssuer().str];
+        }
+        result.meta.issuer = issuerCert;
+  
+        if (issuerCert) {
+          const pubKey = issuerCert.getPublicKey();
+          result.meta.signatureVerified = cert.verifySignature(pubKey);
+        }
+
+        console.log(result);
+        return result;
+      })
+    });
+  }
+
+  // https://github.com/kjur/jsrsasign/wiki/Tutorial-for-accessing-deep-inside-of-ASN.1-structure-by-using-new-ASN1HEX.getIdxbyListEx >> ASN hex access
+  // https://github.com/kjur/jsrsasign/blob/0c47b952931fb52658567e4821593a05266f6e0f/src/x509-1.1.js#L505-L514 >> how to verify cert signature ( where to generate hex/payload )
+  // https://github.com/kjur/jsrsasign/blob/f3d70198b2d923bdeacd2bd8c60605a96101d802/src/rsasign-1.2.js#L274-L291 >> how signature works
+  // https://stackoverflow.com/questions/42691048/whats-the-detail-in-sha1withrsa >> e.g. 
+
+  React.useEffect(() => {
+    loadCert();
+  }, 0);
+
+  return (
+    <div className="container">
+      <span class="badge bg-info">Number of CA Cert Loaded: {Object.keys(caCerts).length}</span>
+      <div className="mb-3">
+        <label for="certs-ta" className="form-label"><h5>Enter Certificate Chains with -----BEGIN/END CERTIFICATE-----</h5></label>
+        <textarea className="form-control" id="certs-ta" name="certs" rows="10"
+                  value={formData.certs} onChange={handleChange}
+        ></textarea>
+        <button className="btn btn-primary m-2" onClick={evaluate}>Evaluate</button>
+        { certs && (<CertTables certs={certs}></CertTables>)}
+      </div>
+    </div>
+  )
+}
+
+class CertHandler {
+
+  newLine = "\n";
+
+  parse(certBundlesText) {
+    const certs = {};
+    const splits = certBundlesText.split(this.newLine);
+
+    let certTemp = [];
+    for (let i = 0; i < splits.length; i++) {
+      let line = splits[i];
+      if (line.length === 0) {
+        continue;
+      }
+
+      certTemp.push(line);
+      if (line.match(/-END CERTIFICATE-/)) {
+        const cert = new X509();
+        cert.readCertPEM(certTemp.join(this.newLine));
+        cert.getInfo(); // validate
+        certs[`${cert.getSubjectString()}`] = cert;
+      }
+    }
+    return certs;
+  }
+
+}
+
+
+
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+  document.getElementById("root")
+)
